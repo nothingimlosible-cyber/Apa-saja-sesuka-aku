e51dbdf547ecc3540b839bb8690e8d9c4d09609f
nothingimlosible-cyber <nothingimlosible@gmail.com>
2026-04-27 22:11:33 +0700

    Update katalog.js

--- api/katalog.js
+++ api/katalog.js
@@ -1,16 +1,17 @@
 export default async function handler(req, res) {
   const url = process.env.SHEET_API;
   
-  if (!url) {
-    return res.status(500).json({ error: "Variabel SHEET_API belum diset" });
-  }
-
   try {
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      method: req.method,
+      headers: { 'Content-Type': 'application/json' },
+      body: req.method === 'POST' ? JSON.stringify(req.body) : null,
+      redirect: 'follow'
+    });
+    
     const data = await response.json();
     return res.status(200).json(data);
   } catch (err) {
-    // Jika Google Sheets error, kita kirim array kosong [] agar web tidak merah
-    return res.status(200).json([]);
+    return res.status(500).json({ error: err.message });
   }
 }
